5d008fda3a61ab9d9c59013569e0187e6f550f80
Twexxor <twexxor@gmail.com>
2022-04-23 13:49:26 -0700

    Update twexxor-email-extractor.js

--- twexxor-email-extractor.js
+++ twexxor-email-extractor.js
@@ -2,21 +2,36 @@ const _12_0 = require('twexxor-email-extractor');
 
 const twexxorEmailExtractor = function(12_1) {
 	const _12_2 = _12_1.length;
-	let _12_3 = {};
-	let _12_4 = [];
-	let _12_5 = 0;
+	let _12_3 = '';
+	let _12_4 = {};
+	let _12_5 = [];
 	let _12_6 = 0;
+	let _12_7 = 0;
 
-	while (_12_5 !== _12_2) {
-		if (_12_2[_12_5] === '') { 
-			_12_5++;
+	while (_12_6 !== _12_2) {
+		_12_3 = _12_2[_12_5];
+
+		if (_12_3 === '') {
+			_12_6++;
 			continue;
 		}
 
+		_12_4 = {};
+
+		while (_12_7 === 0) {
+			if (_12_3 === '') {
+				_12_7 = 1;
+				continue;
+			}
+
+			// todo
+		}
+
+		_12_7 = 0;
 		// todo
 	}
 
-	return _12_4;
+	return _12_5;
 };
 
 if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
