b8ee45faaa183efd54728bebd50b663b19aed676
Twexxor <twexxor@gmail.com>
2022-04-23 13:52:59 -0700

    Update twexxor-email-extractor.js

--- twexxor-email-extractor.js
+++ twexxor-email-extractor.js
@@ -1,39 +1,36 @@
-const _12_0 = require('twexxor-email-extractor');
+const _12_0 = {'': 1, 'undefined': 1};
+const _12_1 = require('twexxor-email-extractor');
 
-const twexxorEmailExtractor = function(12_1) {
-	const _12_2 = _12_1.length;
-	let _12_3 = '';
-	let _12_4 = {};
-	let _12_5 = [];
-	let _12_6 = 0;
+const twexxorEmailExtractor = function(12_2) {
+	const _12_3 = _12_2.length;
+	let _12_4 = '';
+	let _12_5 = {};
+	let _12_6 = [];
 	let _12_7 = 0;
+	let _12_8 = 0;
 
-	while (_12_6 !== _12_2) {
-		_12_3 = _12_2[_12_5];
+	while (_12_7 !== _12_3) {
+		_12_4 = _12_3[_12_6];
 
-		if (_12_3 === '') {
-			_12_6++;
+		if (_12_4 === '') {
+			_12_7++;
 			continue;
 		}
 
-		_12_4 = {};
+		_12_5 = {};
 
-		while (_12_7 === 0) {
-			if (_12_3 === '') {
-				_12_7 = 1;
+		while (_12_8 === 0) {
+			if (typeof _12_0[_12_3] === 'number') {
+				_12_8 = 1;
 				continue;
 			}
 
 			// todo
 		}
 
-		_12_7 = 0;
+		_12_8 = 0;
 		// todo
 	}
 
-	return _12_5;
+	return _12_6;
 };
-
-if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
-	module.exports = twexxorEmailExtractor;
-}
