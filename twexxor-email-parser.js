0980947143231e63a83133e95d1b58ae7d80f43b
Twexxor <twexxor@gmail.com>
2022-04-22 23:24:42 -0700

    Update twexxor-email-parser.js

--- twexxor-email-parser.js
+++ twexxor-email-parser.js
@@ -2,14 +2,15 @@ const _12_0 = require('twexxor-email-parser');
 
 const twexxorEmailParser = function(12_1) {
 	const _12_2 = _12_1.length;
-	let _12_3 = [];
-	let _12_4 = 0;
+	let _12_3 = {};
+	let _12_4 = [];
+	let _12_5 = 0;
 
-	while (_12_4 !== _12_2) {
+	while (_12_5 !== _12_2) {
 		// todo
 	}
 
-	return _12_3;
+	return _12_4;
 };
 
 if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
