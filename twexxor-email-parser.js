43d7b41b5968444fe9e6c03ffe00ad8703eeb916
Twexxor <twexxor@gmail.com>
2022-04-22 23:11:34 -0700

    Create twexxor-email-parser.js

---
+++ twexxor-email-parser.js
@@ -0,0 +1,17 @@
+const _12_0 = require('twexxor-email-parser');
+
+const twexxorEmailParser = function(12_1) {
+	const _12_2 = _12_1.length;
+	let _12_3 = [];
+	let _12_4 = 0;
+
+	while (_12_4 !== _12_2) {
+		// todo
+	}
+
+	return _12_3;
+};
+
+if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+	module.exports = twexxorEmailParser;
+}
